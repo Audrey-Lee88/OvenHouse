7385cf186b120fef5776755f192b88323ecf26e7
Audrey <alee2@olin.edu>
2019-05-06 01:00:52 -0400

    Added a choice between starting the game immediately or reading the instructions Starting the game and having to read the instructions took a long time especially when debugging Decided to implement a choice between starting the game immediately or reading instructions on how to play the game

--- Game/open_scene.py
+++ Game/open_scene.py
@@ -1,35 +1,44 @@
 import pygame
 import show_text as st
 import choose_char as cc
+import instructions as ins
+
+def start():
+    return cc.main()
+def instructions():
+    return ins.main()
 
 def main():
     pygame.init()
-    screen_width, screen_height = 1200, 800
+    screen_width, screen_height = 500, 500
     window = pygame.display.set_mode((screen_width, screen_height))
     running = True
     line_space = 32
     basicfont = pygame.font.Font('8bitoperator.ttf', 20)
+    Rectplace = pygame.draw.rect(window, (255, 0, 0),(100, 200, 100, 100))
+    Rectplace2 = pygame.draw.rect(window, (255, 0, 0),(250, 200, 200, 100))
 
+    st.text_ani('Ovenhouse', (0, 1), line_space,basicfont,window,screen_width, screen_height)
+    st.text_ani('Start', (-100, 3), line_space,basicfont,window,screen_width, screen_height)
+    st.text_ani('instructions', (100, 3), line_space,basicfont,window,screen_width, screen_height)
 
-    st.text_ani('Welcome to OvenHouse', (0, 0), line_space,basicfont,window,screen_width, screen_height)
-    st.text_ani("In this game, you get to choose whether to be Gretel or Hansel.", (0, 1), line_space,basicfont,window,screen_width, screen_height)
-    st.text_ani("Your goal is to escape from the witch's clutches.", (0, 2), line_space,basicfont,window,screen_width, screen_height)
-    st.text_ani('To do so, you must collect two parts of a candy-key.', (0, 3), line_space,basicfont,window,screen_width, screen_height)
-    st.text_ani('Only then, can you can enter the door with a keyhole to escape.', (0, 4), line_space,basicfont,window,screen_width, screen_height)
-    st.text_ani("Do NOT run into the witch's minions (the balls of fire) or the witch herself!", (0, 5), line_space,basicfont,window,screen_width, screen_height)
-    st.text_ani('Well then, GOOD LUCK!', (0, 6), line_space,basicfont,window,screen_width, screen_height)
-
+    pygame.display.update()
     while running:
-        st.blink_text('EXIT THE WINDOW TO PROCEED', (0, 8), (254,254,254),line_space, window, basicfont,screen_width,screen_height)
-        st.blink_text('EXIT THE WINDOW  TO PROCEED', (0, 8), (0,0,0),line_space, window, basicfont, screen_width,screen_height)
+        pos = pygame.mouse.get_pos()
+        pressed1, pressed2, pressed3 = pygame.mouse.get_pressed()
+
+        if Rectplace.collidepoint(pos) and (pressed1 or pressed2 or pressed3):
+            print("Start")
+            return start()
+
+        if Rectplace2.collidepoint(pos) and (pressed1 or pressed2 or pressed3):
+            print("Instructions")
+            running = False
+            return instructions()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                character = cc.main()
-                return character
-
-
 
 if __name__ == "__main__":
     main()
